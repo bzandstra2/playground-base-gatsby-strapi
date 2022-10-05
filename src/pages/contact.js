bdf4d0f658b0b41a919b05d7093bd77dc55b5726
Bartel Zandstra <bart.z@ecgranite.com>
2022-10-05 10:21:04 -0400

    created contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import siteConfig from '../../config/site'
-import Layout from '../../components/layout'
+import Layout from '../components/layout'
+import Seo from '../components/seo/seo'
 import { FaPhone } from '@react-icons/all-files/fa/FaPhone'
 import { GoMail } from '@react-icons/all-files/go/GoMail'
-import { FaFacebook } from '@react-icons/all-files/fa/FaFacebook'
 import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram'
-import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const offices = [
     { id: 1, city: 'Los Angeles', address: ['4556 Brendan Ferry', 'Los Angeles, CA 90210'] },
@@ -22,22 +20,21 @@ const offices = [
     { name: 'News', href: '#' },
   ]
 
-export default function contact() {
+export default function Contact() {
   return (
 
     <Layout>
-
+    <Seo title="Contact" />
 <main className="overflow-hidden">
         {/* Header */}
         <div className="bg-warm-gray-50">
           <div className="py-24 lg:py-32">
             <div className="relative z-10 mx-auto max-w-7xl pl-4 pr-8 sm:px-6 lg:px-8">
               <h1 className="text-4xl font-bold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">
-                Get in touch
+                Get a Free Estimate Today
               </h1>
-              <p className="mt-6 max-w-3xl text-xl text-warm-gray-500">
-                Vel nunc non ut montes, viverra tempor. Proin lectus nibh phasellus morbi non morbi. In elementum urna
-                ut volutpat. Sagittis et vel et fermentum amet consequat.
+              <p className="mt-6 max-w-3xl text-xl text-warm-gray-500 text-center">
+                Send us a message, and one of our design experts will get back to you within 24 hours
               </p>
             </div>
           </div>
@@ -47,29 +44,16 @@ export default function contact() {
         <section className="relative bg-white" aria-labelledby="contact-heading">
           <div className="absolute h-1/2 w-full bg-warm-gray-50" aria-hidden="true" />
           {/* Decorative dot pattern */}
-          <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <svg
+          <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 z-10">
+          <StaticImage
+              src="../images/countertop-install.jpg"
               className="absolute top-0 right-0 z-0 -translate-y-16 translate-x-1/2 transform sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72"
               width={404}
               height={384}
-              fill="none"
               viewBox="0 0 404 384"
               aria-hidden="true"
-            >
-              <defs>
-                <pattern
-                  id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect x={0} y={0} width={4} height={4} className="text-warm-gray-200" fill="currentColor" />
-                </pattern>
-              </defs>
-              <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
-            </svg>
+            />
+              
           </div>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="relative bg-white shadow-xl">
@@ -79,7 +63,7 @@ export default function contact() {
 
               <div className="grid grid-cols-1 lg:grid-cols-3">
                 {/* Contact information */}
-                <div className="relative overflow-hidden bg-gradient-to-b from-teal-500 to-teal-600 py-10 px-6 sm:px-10 xl:p-12">
+                <div className="relative overflow-hidden bg-gradient-to-b from-primary-500 to-primary-600 py-10 px-6 sm:px-10 xl:p-12">
                   {/* Decorative angle backgrounds */}
                   <div className="pointer-events-none absolute inset-0 sm:hidden" aria-hidden="true">
                     <svg
@@ -177,25 +161,24 @@ export default function contact() {
                       </defs>
                     </svg>
                   </div>
-                  <h3 className="text-lg font-medium text-white">Contact information</h3>
-                  <p className="mt-6 max-w-3xl text-base text-teal-50">
-                    Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor
-                    lacus arcu.
+                  <h3 className="text-lg font-medium text-teal-200">Contact information</h3>
+                  <p className="mt-6 max-w-3xl text-base text-white">
+                    Let us know how we can turn your dream project into reality
                   </p>
                   <dl className="mt-8 space-y-6">
                     <dt>
                       <span className="sr-only">Phone number</span>
                     </dt>
                     <dd className="flex text-base text-teal-50">
-                      <FaPhone className="h-6 w-6 flex-shrink-0 text-teal-200" aria-hidden="true" />
+                      <FaPhone className="h-6 w-6 flex-shrink-0 text-white" aria-hidden="true" />
                       <span className="ml-3">+1 (555) 123-4567</span>
                     </dd>
                     <dt>
                       <span className="sr-only">Email</span>
                     </dt>
                     <dd className="flex text-base text-teal-50">
-                      <GoMail className="h-6 w-6 flex-shrink-0 text-teal-200" aria-hidden="true" />
-                      <span className="ml-3">support@workcation.com</span>
+                      <GoMail className="h-6 w-6 flex-shrink-0 text-white" aria-hidden="true" />
+                      <span className="ml-3">info@ecgranite.com</span>
                     </dd>
                   </dl>
                   <ul role="list" className="mt-8 flex space-x-12">
@@ -213,14 +196,8 @@ export default function contact() {
                     </li>
                     <li>
                       <a className="text-teal-200 hover:text-teal-100" href="#">
-                        <span className="sr-only">GitHub</span>
-                        <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-                          <path
-                            fillRule="evenodd"
-                            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
+                        <span className="sr-only">Instagram</span>
+                        <FaInstagram className="h-7 w-7" aria-hidden="true" />
                       </a>
                     </li>
                     <li>
@@ -236,7 +213,7 @@ export default function contact() {
 
                 {/* Contact form */}
                 <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-                  <h3 className="text-lg font-medium text-warm-gray-900">Send us a message</h3>
+                  <h3 className="text-lg font-medium text-warm-gray-900">Tell Us About Your Project</h3>
                   <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                     <div>
                       <label htmlFor="first-name" className="block text-sm font-medium text-warm-gray-900">
@@ -301,9 +278,7 @@ export default function contact() {
                       </div>
                     </div>
                     <div className="sm:col-span-2">
-                      <label htmlFor="subject" className="block text-sm font-medium text-warm-gray-900">
-                        Subject
-                      </label>
+        
                       <div className="mt-1">
                         <input
                           type="text"
@@ -316,7 +291,7 @@ export default function contact() {
                     <div className="sm:col-span-2">
                       <div className="flex justify-between">
                         <label htmlFor="message" className="block text-sm font-medium text-warm-gray-900">
-                          Message
+                          How Can We Help You?
                         </label>
                         <span id="message-max" className="text-sm text-warm-gray-500">
                           Max. 500 characters
@@ -336,7 +311,7 @@ export default function contact() {
                     <div className="sm:col-span-2 sm:flex sm:justify-end">
                       <button
                         type="submit"
-                        className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-teal-500 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 sm:w-auto"
+                        className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-primary-500 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 sm:w-auto"
                       >
                         Submit
                       </button>
