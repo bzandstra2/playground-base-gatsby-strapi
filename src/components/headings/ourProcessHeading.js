4f0ed029201e7218efbf1836921930fa1196368a
Bartel Zandstra <bart.z@ecgranite.com>
2022-10-05 16:25:25 -0400

    small change

--- src/components/headings/ourProcessHeading.js
+++ src/components/headings/ourProcessHeading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ourProcessHeading() {
   return (
-    <div className="p-10 flex justify-center align-center">
+    <div className="p-6 flex justify-center align-center">
         <h2 className="text-6xl text-center">Our Process</h2>
     </div>
   )
